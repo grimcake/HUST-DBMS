8521e025690e3c2e58d4524c3e5cde5b30cb0472
grimcake <994459124@qq.com>
2019-12-19 00:07:17 +0800

    update

--- RM_Manager.cpp
+++ RM_Manager.cpp
@@ -1,48 +1,488 @@
 #include "stdafx.h"
 #include "RM_Manager.h"
+#include "PF_Manager.h"
+#include "BitMap.h"
 #include "str.h"
+
+#include <assert.h>
+#include <iostream>
 
 
 RC OpenScan(RM_FileScan *rmFileScan,RM_FileHandle *fileHandle,int conNum,Con *conditions)//初始化扫描
 {
+	RC rc;
+	if (rmFileScan->bOpen == true)
+	{
+		return RM_FSOPEN;
+	}
+	// 如果文件没有数据
+	if (fileHandle->pfFileHandle.pFileSubHeader->nAllocatedPages <= 2)
+	{
+		rmFileScan->pn = rmFileScan->sn = 0;
+		return SUCCESS;
+	}
+	// 初始化
+	rmFileScan->bOpen = true;
+	rmFileScan->pRMFileHandle = fileHandle;
+	rmFileScan->conNum = conNum;
+	rmFileScan->conditions = conditions;
+	// 设置扫描的首页面
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, 2, &rmFileScan->PageHandle))
+	{
+		return rc;
+	}
+	rmFileScan->pn = 2;
+	rmFileScan->sn = 0;
 	return SUCCESS;
 }
 
 RC GetNextRec(RM_FileScan *rmFileScan,RM_Record *rec)
 {
-	return SUCCESS;
+	if (rmFileScan->bOpen == false)
+	{
+		return RM_FSCLOSED;
+	}
+	// 文件无内容
+	if (rmFileScan->pn == 0)
+	{
+		return RM_NOMORERECINMEM;
+	}
+	while (true)
+	{
+		if (GetThisPage(&rmFileScan->pRMFileHandle->pfFileHandle, rmFileScan->pn, &rmFileScan->PageHandle))
+		{
+			return FAIL;
+		}
+		// 寻找下一个有内容的插槽
+		BitMap bm(rmFileScan->pRMFileHandle->recOffset, rmFileScan->PageHandle.pFrame->page.pData);
+		while (bm.FirstBit(rmFileScan->sn, 1) != -1)
+		{ 
+			Con* condition;
+			bool correct = true;
+			// 该条记录所在的位置
+			int recOffset = rmFileScan->pRMFileHandle->recOffset + rmFileScan->sn*rmFileScan->pRMFileHandle->recSize;
+			char* recAddr = rmFileScan->PageHandle.pFrame->page.pData + recOffset;
+
+			int leftVal, rightVal;
+			int leftF, rightF;
+			char *leftStr, *rightStr;
+
+			// 遍历判断每一个条件
+			int conNumber;
+			for (conNumber = 0; conNumber < rmFileScan->conNum; conNumber++)
+			{
+				condition = (Con*)(rmFileScan->conditions + conNumber);
+				switch (condition->attrType)
+				{
+				case ints:
+					leftVal = (condition->bLhsIsAttr == 1) ? *((int*)(recAddr + condition->LattrOffset)) : *((int*)condition->Lvalue);
+					if (condition->compOp == NO_OP)
+					{
+						if (leftVal == 0)
+						{
+							correct = false;
+							break;
+						}
+						else
+						{
+							break;
+						}
+					}
+					rightVal = (condition->bRhsIsAttr == 1) ? *((int *)(recAddr + condition->RattrOffset)) : *((int *)condition->Rvalue);
+					correct = CmpVal(leftVal, rightVal, condition->compOp);
+					break;
+				case floats:
+					leftVal = (condition->bLhsIsAttr == 1) ? *((float*)(recAddr + condition->LattrOffset)) : *((float*)condition->Lvalue);
+					if (condition->compOp == NO_OP)
+					{
+						if (leftVal == 0)
+						{
+							correct = false;
+							break;
+						}
+						else
+						{
+							break;
+						}
+					}
+					rightVal = (condition->bRhsIsAttr == 1) ? *((float *)(recAddr + condition->RattrOffset)) : *((float *)condition->Rvalue);
+					correct = CmpVal(leftVal, rightVal, condition->compOp);
+					break;
+				case chars:
+					if (condition->compOp == NO_OP)
+					{
+						correct = false;
+						break;
+					}
+					leftStr = (condition->bLhsIsAttr == 1) ? (recAddr + condition->LattrOffset) : (char *)condition->Lvalue;
+					rightStr = (condition->bRhsIsAttr == 1) ? (recAddr + recOffset + condition->RattrOffset) : (char *)condition->Rvalue;
+					//比较两个字符串
+					correct = CmpStr(leftStr, rightStr, condition->compOp);
+					break;
+				}
+				if (!correct)
+				{
+					break;
+				}
+			}
+			// 满足所有的比较条件
+			if (conNumber == rmFileScan->conNum)
+			{
+				rec->bValid = true;
+				rec->pData = recAddr;
+				rec->rid.bValid = true;
+				rec->rid.pageNum = rmFileScan->pn;
+				rec->rid.slotNum = rmFileScan->sn;
+				rmFileScan->sn++;
+				return SUCCESS;
+			}
+			else
+			{
+				rmFileScan->sn++;
+			}
+		}
+		UnpinPage(&rmFileScan->PageHandle);
+		if (rmFileScan->pRMFileHandle->pageBitMap->FirstBit(rmFileScan->pn + 1, 1) == -1)
+		{
+			// 之后没有分配使用的页面
+			return RM_NOMORERECINMEM;
+		}
+		// 找下一个有内容的页
+		rmFileScan->pn = rmFileScan->pRMFileHandle->pageBitMap->FirstBit(rmFileScan->pn + 1, 1);
+		if (rmFileScan->pn == -1)
+		{
+			return FAIL;
+		}
+		GetThisPage(&rmFileScan->pRMFileHandle->pfFileHandle, rmFileScan->pn, &rmFileScan->PageHandle);
+		rmFileScan->sn = 0;
+	}
+	return FAIL;
 }
 
 RC GetRec (RM_FileHandle *fileHandle,RID *rid, RM_Record *rec) 
 {
+	rec->bValid = false;
+	if (fileHandle->pageBitMap->Test(rid->pageNum) == 0 || rid->pageNum<2 || rid->slotNum>fileHandle->recPerPage-1)
+	{
+		return RM_INVALIDRID;
+	}
+	// 获取rid所在页
+	PF_PageHandle pfPageHandle;
+	RC rc;
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, rid->pageNum, &pfPageHandle))
+	{
+		return rc;
+	}
+	// 在该页位图中判断一下rid代表的记录是否有效
+	BitMap bm(fileHandle->recOffset, &pfPageHandle.pFrame->page.pData[0]);
+	if (bm.Test(rid->slotNum) == 0)
+	{
+		return RM_INVALIDRID;
+	}
+
+	// 获取rid所在插槽的地址
+	rec->rid.bValid = true;
+	rec->rid.pageNum = rid->pageNum;
+	rec->rid.slotNum = rid->slotNum;
+	// 插槽只存pData不存rid
+	rec->pData = &pfPageHandle.pFrame->page.pData[fileHandle->recOffset+rec->rid.slotNum*fileHandle->recSize];
+	
+
+
+	UnpinPage(&pfPageHandle);
 	return SUCCESS;
 }
 
+// 分为几种情况：
+// 1.存在已分配的未满页：插入记录
+// 2.不存在已分配的未满页
+// 2.1 存在空闲页：分配新页：插入记录
+// 2.2 不存在空闲页：返回FAIL
 RC InsertRec (RM_FileHandle *fileHandle,char *pData, RID *rid)
 {
+	// 判断满页情况
+	RC rc;
+	PF_PageHandle pfPageHandle;
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, 1, &pfPageHandle))
+	{
+		return rc;
+	}
+	int insertPageNum = -1;  
+	for (int i = 2; i < fileHandle->bitMapLength; i++)
+	{
+		// 找到第一个已分配且未满的页
+		// TODO:这里可以优化一下查找算法
+		if (fileHandle->pageBitMap->Test(i) == 1 && fileHandle->recordBitMap->Test(i) == 0)
+		{
+			insertPageNum = i;
+			break;
+		}
+	}
+	
+	// 1.存在已分配的未满页：插入记录
+	if (insertPageNum !=-1)
+	{
+		// 找到该页第一个空闲的插槽
+		PF_PageHandle tmpPage;
+		GetThisPage(&fileHandle->pfFileHandle, insertPageNum, &tmpPage);
+		// 初始化一个位图，否则位图中的data没有值
+		BitMap bm(fileHandle->recOffset, &tmpPage.pFrame->page.pData[0]);
+		int insertPagePos = bm.FirstBit(0, 0);
+		assert(insertPagePos != -1);
+		memcpy(&(tmpPage.pFrame->page.pData[fileHandle->recOffset])+insertPagePos*sizeof(fileHandle->recSize), pData, fileHandle->recSize);
+		// 修改本页位图
+		bm.Set(insertPagePos);
+
+		MarkDirty(&tmpPage);
+		UnpinPage(&tmpPage);
+	}
+	// 2.不存在已分配的未满页
+	else
+	{
+		// 判断是否存在空闲页
+		if (fileHandle->pageBitMap->hasZero() == true)
+		{
+			// 2.1 存在空闲页：分配新页：插入记录
+			PF_PageHandle newPage;
+			if (rc = AllocatePage(&fileHandle->pfFileHandle, &newPage))
+			{
+				return rc;
+			}
+			else
+			{
+				memcpy(&newPage.pFrame->page.pData[fileHandle->recOffset], pData, fileHandle->recSize);
+				// 修改本页位图
+				BitMap bm(fileHandle->recOffset, &newPage.pFrame->page.pData[0]);
+				bm.Set(0);
+
+				MarkDirty(&newPage);
+				UnpinPage(&newPage);
+			}
+		}
+		else
+		{
+			// 2.2 不存在空闲页 
+			return FAIL;
+		}
+	}
+	
 	return SUCCESS;
 }
 
+// 1. 找到记录所在的页面，插槽号
+// 2. 修改本页面的位图
+// 3. 如果删除后该页变为未满但有记录，则修改记录信息控制页该页面的状态
+// 4. 如果删除后该页不再有记录，则修改信息控制页该页面的状态，并将其置为空闲
 RC DeleteRec (RM_FileHandle *fileHandle,const RID *rid)
 {
+	RC rc;
+	// 1.
+	int pageNum = rid->pageNum;
+	int slotNum = rid->slotNum;
+	PF_PageHandle pfPageHandle;
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, pageNum, &pfPageHandle))
+	{
+		return rc;
+	}
+	BitMap bm(fileHandle->recOffset, pfPageHandle.pFrame->page.pData);
+	bool isFull = bm.hasZero();
+	
+	// 2.
+	bm.Reset(slotNum);
+	assert(isFull);
+	// 3.
+	fileHandle->recordBitMap->Reset(pageNum);
+	PF_PageHandle recPageHandle;
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, 1, &recPageHandle))
+	{
+		return rc;
+	}
+	BitMap recbm(fileHandle->bitMapLength, &recPageHandle.pFrame->page.pData[0] + sizeof(RM_FileSubHeader));
+	recbm.Reset(pageNum);
+
+	int isEmpty = bm.FirstBit(0, 1);
+	if (isEmpty == -1)
+	{
+		// 4.
+		DisposePage(&fileHandle->pfFileHandle, pageNum);
+	}
+	
+	MarkDirty(&recPageHandle);
+	UnpinPage(&recPageHandle);
+	MarkDirty(&pfPageHandle);
+	UnpinPage(&pfPageHandle);
 	return SUCCESS;
 }
 
 RC UpdateRec (RM_FileHandle *fileHandle,const RM_Record *rec)
 {
+	RC rc;
+	int pageNum = rec->rid.pageNum;
+	int slotNum = rec->rid.slotNum;
+	PF_PageHandle pfPageHandle;
+	if (rc = GetThisPage(&fileHandle->pfFileHandle, pageNum, &pfPageHandle))
+	{
+		return rc;
+	}
+	memcpy(&pfPageHandle.pFrame->page.pData[fileHandle->recOffset] + rec->rid.slotNum*fileHandle->recSize, rec->pData, fileHandle->recSize);
+	MarkDirty(&pfPageHandle);
+	UnpinPage(&pfPageHandle);
 	return SUCCESS;
 }
 
 RC RM_CreateFile (char *fileName, int recordSize)
 {
+	assert(fileName);
+	RC rc;
+
+	// 打开文件，获取页面句柄
+	PF_FileHandle pfFileHandle;
+
+	if ((rc = CreateFile(fileName)))
+	{
+		return rc;
+	}
+	if ((rc = openFile(fileName, &pfFileHandle)))
+	{
+		return rc;
+	}
+	// 页面信息管理页
+	
+	
+
+	// 根据页面句柄生成记录信息控制页(第1页)
+	PF_PageHandle pfPageHandle;
+
+	if ((rc = AllocatePage(&pfFileHandle, &pfPageHandle)))
+	{
+		return rc;
+	}
+	assert(pfPageHandle.pFrame->page.pageNum == 1);
+
+	// 从pData[0]开始，存放一个数据结构RM_FileSubHeader记录控制信息，并进行初始化
+	RM_FileSubHeader *rmFileSubHeader;
+
+	rmFileSubHeader = (RM_FileSubHeader *)pfPageHandle.pFrame->page.pData;
+	rmFileSubHeader->nRecords = 0;
+	rmFileSubHeader->recordSize = recordSize;
+	// 每个页面从pData[0]开始有一个recordPerPage/8大小的位图，用于管理页面记录插槽使用情况
+	// recordPerPage*recordSize+0.125*recordPerPage=PF_PAGE_SIZE
+	rmFileSubHeader->recordsPerPage = (PF_PAGE_SIZE) / (recordSize+0.125); 
+	// 保证firstRecordOffset为整数，即recordsPerPage向下取8的倍数
+	rmFileSubHeader->recordsPerPage = ((int)(rmFileSubHeader->recordsPerPage / 8)) * 8;
+	rmFileSubHeader->firstRecordOffset = rmFileSubHeader->recordsPerPage/8;
+	if ((rc = MarkDirty(&pfPageHandle)) || (rc = UnpinPage(&pfPageHandle)) || (rc = CloseFile(&pfFileHandle)))
+	{
+		return rc;
+	}
+
 	return SUCCESS;
 }
 RC RM_OpenFile(char *fileName, RM_FileHandle *fileHandle)
 {
+	assert(fileHandle);
+	RC rc;
+	
+	// 判断文件打开情况
+	if (fileHandle->bOpen)
+	{
+		return RM_FHOPENNED;
+	}
+	if ((rc = openFile(fileName, &fileHandle->pfFileHandle)))
+	{
+		return rc;
+	}
+	fileHandle->bOpen = true;
+
+	// 对fileHandle进行有关信息的获取
+	// 1. 获取记录信息控制页
+	PF_PageHandle pfPageHandle;
+	if ((rc = GetThisPage(&fileHandle->pfFileHandle, 1, &pfPageHandle)))
+	{
+		CloseFile(&fileHandle->pfFileHandle);
+		return rc;
+	}
+	fileHandle->recPerPage = ((RM_FileSubHeader*)&pfPageHandle.pFrame->page.pData[0])->recordsPerPage;
+	fileHandle->recOffset = ((RM_FileSubHeader*)&pfPageHandle.pFrame->page.pData[0])->firstRecordOffset;
+	fileHandle->recSize = ((RM_FileSubHeader*)&pfPageHandle.pFrame->page.pData[0])->recordSize;
+	fileHandle->bitMapLength = PF_PAGE_SIZE - sizeof(RM_FileSubHeader);
+	// 2. 设置记录信息控制页位图
+	fileHandle->recordBitMap = new BitMap(fileHandle->bitMapLength, &pfPageHandle.pFrame->page.pData[sizeof(RM_FileSubHeader)]);
+
+	// 3. 设置页面信息控制页位图
+	fileHandle->pageBitMap = new BitMap(fileHandle->bitMapLength, fileHandle->pfFileHandle.pBitmap);
+
+	UnpinPage(&pfPageHandle);
+
 	return SUCCESS;
 }
 
 RC RM_CloseFile(RM_FileHandle *fileHandle)
 {
+	assert(fileHandle);
+	RC rc;
+	if (fileHandle->pfFileHandle.bopen == false)
+	{
+		return RM_FHCLOSED;
+	}
+	if (rc = CloseFile(&fileHandle->pfFileHandle))
+	{
+		return rc;
+	}
+	fileHandle->bOpen = false;
+
+	if (fileHandle->pageBitMap)
+	{
+		delete fileHandle->pageBitMap;
+	}
+	if (fileHandle->recordBitMap)
+	{
+		delete fileHandle->recordBitMap;
+	}
 	return SUCCESS;
 }
+
+
+bool CmpStr(char *left, char *right, CompOp Op)
+{
+	int cmpResult = strcmp(left, right);
+	switch (Op)
+	{
+	case EQual:
+		return (cmpResult == 0) ? true : false;
+	case LessT:
+		return (cmpResult < 0) ? true : false;
+	case GreatT:
+		return (cmpResult > 0) ? true : false;
+	case NEqual:
+		return (cmpResult == 0) ? false : true;
+	case LEqual:
+		return (cmpResult == 0 || cmpResult < 0) ? true : false;
+	case GEqual:
+		return (cmpResult == 0 || cmpResult > 0) ? true : false;
+	default:
+		return false;
+	}
+}
+
+bool CmpVal(float left, float right, CompOp Op)
+{
+	switch (Op)
+	{
+	case EQual:
+		return (left == right);
+	case LessT:
+		return (left < right);
+	case GreatT:
+		return (left > right);
+	case NEqual:
+		return (left != right);
+	case LEqual:
+		return (left <= right);
+	case GEqual:
+		return (left >= right);
+	default:
+		return false;
+	}
+}
+
--- RM_Manager.h
+++ RM_Manager.h
@@ -3,6 +3,7 @@
 
 #include "PF_Manager.h"
 #include "str.h"
+#include "BitMap.h"
 
 typedef int SlotNum;
 
@@ -18,20 +19,34 @@ typedef struct{
 	char *pData; 		 //记录所存储的数据 
 }RM_Record;
 
+typedef struct {
+	int nRecords;   // 当前文件中包含的记录数
+	int recordSize;  // 每个记录的大小
+	int recordsPerPage; // 每个页面可以装载的记录数量
+	int firstRecordOffset; //每页第一个记录在数据区中的开始位置
+}RM_FileSubHeader;
+
 
 typedef struct
 {
-	int bLhsIsAttr,bRhsIsAttr;//左、右是属性（1）还是值（0）
-	AttrType attrType;
-	int LattrLength,RattrLength;
-	int LattrOffset,RattrOffset;
-	CompOp compOp;
-	void *Lvalue,*Rvalue;
+	int bLhsIsAttr,bRhsIsAttr;  //条件的左、右分别是属性（1）还是值（0）
+	AttrType attrType;   //该条件中数据的类型
+	int LattrLength,RattrLength;  //若是属性的话，表示属性的长度
+	int LattrOffset,RattrOffset;  //若是属性的话，表示属性的偏移量
+	CompOp compOp;  //比较操作符
+	void *Lvalue,*Rvalue;  //若是值的话，指向对应的值
 }Con;
 
 typedef struct{//文件句柄
 	bool bOpen;//句柄是否打开（是否正在被使用）
 	//需要自定义其内部结构
+	int recPerPage;  // 与RM_FileSubHeader中的recordsPerPage相等，在RM_OpenFile时赋值
+	int bitMapLength;  // 页面位图和记录位图的字节大小
+	int recOffset; // 等于recPerPage/8
+	int recSize; // 一条记录的大小
+	BitMap *pageBitMap;   // 页面位图，位于第0页
+	BitMap *recordBitMap;   // 记录位图， 位于第1页
+	PF_FileHandle pfFileHandle;
 }RM_FileHandle;
 
 typedef struct{
@@ -45,25 +60,50 @@ typedef struct{
 }RM_FileScan;
 
 
-
+// 获取一个符合扫描条件的记录。如果该方法成功，
+// 返回值rec应包含记录副本及记录标识符。
+// 如果没有发现满足扫描条件的记录，则返回RM_EOF。
 RC GetNextRec(RM_FileScan *rmFileScan,RM_Record *rec);
 
+// 打开一个文件扫描。
+// 本函数利用从第二个参数开始的所有输入参数初始化一个由参数rmFileScan指向的文件扫描结构，
+// 在使用中，用户应先调用此函数初始化文件扫描结构，
+// 然后再调用GetNextRec函数来逐个返回文件中满足条件的记录。
+// 如果条件数量conNum为0，则意味着检索文件中的所有记录。
+// 如果条件不为空，则要对每条记录进行条件比较，只有满足所有条件的记录才被返回。
 RC OpenScan(RM_FileScan *rmFileScan,RM_FileHandle *fileHandle,int conNum,Con *conditions);
 
+// 关闭一个文件扫描，释放相应的资源。
 RC CloseScan(RM_FileScan *rmFileScan);
 
+// 更新指定文件中的记录，
+// rec指向的记录结构中的rid字段为要更新的记录的标识符，
+// pData字段指向新的记录内容。
 RC UpdateRec (RM_FileHandle *fileHandle,const RM_Record *rec);
 
+// 从指定文件中删除标识符为rid的记录。
 RC DeleteRec (RM_FileHandle *fileHandle,const RID *rid);
 
+// 插入一个新的记录到指定文件中，pData为指向新纪录内容的指针，
+// 返回该记录的标识符rid。
 RC InsertRec (RM_FileHandle *fileHandle, char *pData, RID *rid); 
 
+// 获取指定文件中标识符为rid的记录内容到rec指向的记录结构中。
 RC GetRec (RM_FileHandle *fileHandle, RID *rid, RM_Record *rec); 
 
+// 关闭给定句柄对应的记录文件。
 RC RM_CloseFile (RM_FileHandle *fileHandle);
 
+// 根据文件名打开指定的记录文件，返回其文件句柄指针。
 RC RM_OpenFile (char *fileName, RM_FileHandle *fileHandle);
 
+// 创建一个名为fileName的记录文件，
+// 该文件中每条记录的大小为recordSize
 RC RM_CreateFile (char *fileName, int recordSize);
+
+bool CmpVal(float left, float right, CompOp Op);
+bool CmpStr(char *left, char *right, CompOp Op);
+
+
 
 #endif
